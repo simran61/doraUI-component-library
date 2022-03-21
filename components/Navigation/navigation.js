57dd8c1b2719028d2faafac990b9fcdb24f432d5
Simran Gangwani <simrangngwani3107@gmail.com>
2022-03-21 16:24:11 +0530

    refactor: changes classes name & updated iframes

--- components/Navigation/navigation.js
+++ components/Navigation/navigation.js
@@ -1,16 +1,16 @@
-const hambergBtn = document.querySelector(".drawer-hamberg-btn");
-const drawer = document.querySelector(".drawer-container");
-const closBtn = document.querySelector(".drawer-close");
+const hamburgerBtn = document.querySelector(".hamburger_menu_drawer");
+const drawer = document.querySelector(".drawer_container");
+const closBtn = document.querySelector(".drawer_close");
 
 
-hambergBtn.addEventListener("click",()=>{
-    drawer.classList.add("drawer-trans-on");
-    drawer.classList.remove("drawer-trans-off");
+hamburgerBtn.addEventListener("click",()=>{
+    drawer.classList.add("transition_start");
+    drawer.classList.remove("transition_end");
 })
 
 function closeDrawer(){
-    drawer.classList.remove("drawer-trans-on");
-    drawer.classList.add("drawer-trans-off");
+    drawer.classList.remove("transition_start");
+    drawer.classList.add("transition_end");
 }
 
 closBtn.addEventListener("click",closeDrawer)
