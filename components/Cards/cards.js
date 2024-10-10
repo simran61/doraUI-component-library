eb5d656a76692a6869c318bbda941f11e18d876f
simran61 <simrangangwani3107@gmail.com>
2024-10-10 17:17:54 +0530

    dismissable card

--- components/Cards/cards.js
+++ components/Cards/cards.js
@@ -1,13 +1,20 @@
-const dismiss1= document.querySelector('.dismiss1');
-const dismissCard1= document.querySelector('.dismiss_card1');
+const dismiss1 = document.querySelector(".dismiss1");
+const dismissCard1 = document.querySelector(".dismiss_card1");
 
-const dismiss2= document.querySelector('.dismiss2');
-const dismissCard2= document.querySelector('.dismiss_card2');
+const dismiss2 = document.querySelector(".dismiss2");
+const dismissCard2 = document.querySelector(".dismiss_card2");
 
-dismiss1.addEventListener('click',()=>{
-    dismissCard1.style.display="none";
-})
+const dismiss3 = document.querySelector(".dismiss3");
+const dismissCard3 = document.querySelector(".dismiss_card3");
 
-dismiss2.addEventListener('click',()=>{
-    dismissCard2.style.display="none";
-})
+dismiss1.addEventListener("click", () => {
+  dismissCard1.style.display = "none";
+});
+
+dismiss2.addEventListener("click", () => {
+  dismissCard2.style.display = "none";
+});
+
+dismiss3.addEventListener("click", () => {
+  dismissCard3.style.display = "none";
+});
